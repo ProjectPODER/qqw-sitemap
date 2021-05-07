ed3b346c04a39618a970d50ee7072ca850a47179
Fernando Matzdorf <fernando@rindecuentas.org>
2021-05-07 13:43:05 -0500

    Fixed push script.

--- push.sh
+++ push.sh
@@ -2,23 +2,27 @@
 export GITHUB_USER
 export GITHUB_PASS
 
+G_USER="$(echo "${GITHUB_USER}" | tr -d '[[:space:]]')"
+G_PASS="$(echo "${GITHUB_PASS}" | tr -d '[[:space:]]')"
+REPO="https://${G_USER}:${G_PASS}@github.com/ProjectPODER/qqw-generated-assets.git"
+echo $REPO
 CURRENTDATE=`date +"%Y.%m.%d-%H.%M"`
 echo Current Date and Time is: ${CURRENTDATE}
 
 # Start from scratch
-rm -rf qqw-generated-assets
-mkdir qqw-generated-assets
-git clone "https://$GITHUB_USER:$GITHUB_PASS@github.com/ProjectPODER/qqw-generated-assets.git"
+rm -rf $POPPINS_SCRIPTS_DIR/qqw-sitemap/qqw-generated-assets
+mkdir $POPPINS_SCRIPTS_DIR/qqw-sitemap/qqw-generated-assets
+git clone $REPO
 echo Cloned generated assets
 
 # Replace sitemap files with newly generated ones
-cd qqw-generated-assets
-rm -f *.xml
-cp $POPPINS_SCRIPTS_DIR/qqw-sitemap/*.xml .
+rm -f $POPPINS_SCRIPTS_DIR/qqw-sitemap/qqw-generated-assets/*.xml
+mv $POPPINS_SCRIPTS_DIR/qqw-sitemap/*.xml $POPPINS_SCRIPTS_DIR/qqw-sitemap/qqw-generated-assets/
 echo Copied files to repo
 
 # Commit and push
+cd $POPPINS_SCRIPTS_DIR/qqw-sitemap/qqw-generated-assets
 git add .
 git commit -am "${CURRENTDATE}"
-git "push https://$GITHUB_USER:$GITHUB_PASS@github.com/ProjectPODER/qqw-generated-assets.git" --all
+git push $REPO --all
 echo Pushed repo
